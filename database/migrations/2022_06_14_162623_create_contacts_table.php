eca14df1cc3057aaa532ccb47a775a026b957edf
aseel1999 <aseelmaysoum@gmail.com>
2023-07-02 16:28:27 +0300

    me

--- database/migrations/2022_06_14_162623_create_contacts_table.php
+++ database/migrations/2022_06_14_162623_create_contacts_table.php
@@ -19,7 +19,7 @@ class CreateContactsTable extends Migration
             $table->string('f_name');
             //$table->string('l_name');
             $table->string('phone');
-            $table->enum('type',['owner','director','site Manager','landlord','agent','finance Manager']);
+            $table->enum('type',['director','site Manager','landlord','agent','finance Manager']);
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
         });
