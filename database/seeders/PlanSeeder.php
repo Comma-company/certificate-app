25097a13002b3a5f766522d367e2dbee5e4541b0
aseel1999 <aseelmaysoum@gmail.com>
2023-07-10 16:26:31 +0300

    all

--- database/seeders/PlanSeeder.php
+++ database/seeders/PlanSeeder.php
@@ -18,48 +18,49 @@ class PlanSeeder extends Seeder
                 'name' => 'Electrical Certificates', 
                 'stripe_plan' => 'price_1NSEnIE2sCQWSLCAuYftfKel', 
                 'price' => 6.99, 
-                'intrval'=>'monthly',
+                'interval'=>'monthly',
                 
             ],
             [
                 'name' => 'Electrical Certificates', 
                 'stripe_plan' => 'price_1NSEnvE2sCQWSLCA4qKRGbcm', 
                 'price' => 69.99, 
-                'intrval'=>'yearly',
+                'interval'=>'yearly',
                 
             ],
             [
                 'name' => 'Gas', 
                 'stripe_plan' => 'price_1NSEobE2sCQWSLCAADyyEHpA', 
                 'price' => 6.99, 
-                'intrval'=>'monthly',
+                'interval'=>'monthly',
                 
             ],
             [
                 'name' => 'Gas', 
                 'stripe_plan' => 'price_1NSEp5E2sCQWSLCAWst1M1Hp', 
                 'price' =>69.99,
-                'intrval'=>'yearly', 
+                'interval'=>'yearly', 
                 
             ],
             [
                 'name' => 'Gas & Electric', 
                 'stripe_plan' => 'price_1NSEpyE2sCQWSLCAsIUdLcKg', 
                 'price' =>10.49, 
-                'intrval'=>'monthly',
+                'interval'=>'monthly',
                 
             ],
             [
                 'name' => 'Gas & Electric', 
                 'stripe_plan' => 'price_1NSEqcE2sCQWSLCA1WvNFme4', 
                 'price' =>125.88,
-                'intrval'=>'yearly',
+                'interval'=>'yearly',
                 
             ],
             [
                 'name' => 'free', 
                 'stripe_plan' => 'price_1NSEkhE2sCQWSLCAGK6joBPt', 
                 'price' =>0.00,
+                'interval'=>'monthly',
                 
                 
             ],
