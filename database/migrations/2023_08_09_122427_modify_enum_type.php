82cb226c9b1eec1a9a2c0a92a4d3fe83243a9c1a
luayjal <luayjal1@gmail.com>
2023-08-09 16:04:31 +0300

    update migration

--- database/migrations/2023_08_09_122427_modify_enum_type.php
+++ database/migrations/2023_08_09_122427_modify_enum_type.php
@@ -26,7 +26,7 @@ class ModifyEnumType extends Migration
      */
     public function down()
     {
-        DB::statement("ALTER TABLE site_contacts MODIFY COLUMN type enum('director', 'siteManager', 'landlord', 'agent', 'financeManager', 'tenant')");
         DB::statement("ALTER TABLE contacts MODIFY COLUMN type enum('agent','landlord')");
+        DB::statement("ALTER TABLE site_contacts MODIFY COLUMN type enum('director', 'siteManager', 'landlord', 'agent', 'financeManager', 'tenant')");
     }
 }
